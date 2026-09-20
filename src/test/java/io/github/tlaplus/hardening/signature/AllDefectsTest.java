cf5bc28130b83bb6c6014d4401359dd75d3a1c9c
Igor Konnov <igor@konnov.phd>
2026-09-20 05:01:40 +0200

    fix 'make package'

--- src/test/java/io/github/tlaplus/hardening/signature/AllDefectsTest.java
+++ src/test/java/io/github/tlaplus/hardening/signature/AllDefectsTest.java
@@ -41,7 +41,8 @@ class AllDefectsTest {
             "findings/apalache-printer/apalache-printer-004.md",
             "findings/apalache-printer/apalache-printer-005.md",
             "findings/apalache-printer/apalache-printer-006.md",
-            "findings/apalache-printer/apalache-printer-007.md");
+            "findings/apalache-printer/apalache-printer-007.md",
+            "findings/TLC/tlc-013.md");
 
     private final SignatureShapes shapes = new SignatureShapes();
     private final TlaTypedScopeUncheckedBuilder builder = shapes.builder;
